e6fc94e9fd63283456f85a172e85a0e01fe69878
Kragada <35291972+Kragada@users.noreply.github.com>
2021-11-25 13:40:41 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CircleCIdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
